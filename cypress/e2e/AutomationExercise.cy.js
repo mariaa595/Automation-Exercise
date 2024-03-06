9ce12decc09cdfa54407eb126e4e22b2e146990e
mariaa595 <mariakhan5950@gmail.com>
2024-03-06 10:41:48 +0500

    latest changes

--- cypress/e2e/AutomationExercise.cy.js
+++ cypress/e2e/AutomationExercise.cy.js
@@ -93,7 +93,7 @@ describe("AutomationExercise", () => {
     cy.get('[data-qa="continue-button"]').click();
   });
 
-  it("TC 02 Login User with correct email and password", () => {
+  it.skip("TC 02 Login User with correct email and password", () => {
     //3. Verify that home page is visible successfully
     cy.get('a[href="/login"]').should("be.visible");
 
